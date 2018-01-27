961db95e7ed992956ae0de6377847b933d15cfd3
Kevin Semple <KSemp@falconpunch-8.local>
2018-01-27 13:58:54 -0800

    Created ActionManager for vive controller gestures

---
+++ Assets/ActionManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionManager : MonoBehaviour {
+
+	public bool actionCube1;
+	public bool actionCube2;
+	public Material myMaterial;
+
+	private float elapsedTime;
+	private float cube1Timer;
+	private float cube2Timer;
+
+	// Use this for initialization
+	void Start () {
+		actionCube1 = false;
+		actionCube2 = false;
+
+		cube1Timer = 0;
+		cube2Timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsedTime += Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag("ActionCube1")){
+			actionCube1 = true;
+		}
+		if(other.CompareTag("ActionCube2")){
+			actionCube2 = true;
+		}
+		other.GetComponent<MeshRenderer> ().material = myMaterial;
+	}
+}
--- Assets/Scripts/RoboArm.cs
+++ Assets/Scripts/RoboArm.cs
@@ -4,44 +4,21 @@ using UnityEngine;
 
 public class RoboArm : MonoBehaviour {
 
-	public Transform armSpawn;
-	public float thrust = 10f;
 
-	private bool punching;
-	private bool grabbing;
-
-	private Rigidbody rgbd;
 
 
 
 	// Use this for initialization
 	void Start () {
-		punching = false;
-		grabbing = false;
-
-		rgbd = gameObject.GetComponent<Rigidbody> ();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButton("Fire1")){
-			Punch ();
-		}
-		print (rgbd.position);
+		
 	}
 
-	void Punch(){
-		//play punch animation
 
-		//For testing
-		//x-direction is forward
-		rgbd.MovePosition (rgbd.position+Vector3.right * thrust);
-	}
-
-	IEnumerator RetractArm(){
-		yield return new WaitForSeconds(2);
-		rgbd.MovePosition (armSpawn);
-	}
 		
 
 }
